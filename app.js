f257a3549d0ce54c7c238806b16f9351e530cf3f
margi <margipat@usc.edu>
2014-02-14 13:27:12 -0800

    changes to send queries to sparql endpoint sequentially and sychronously

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var rdfstore = require('rdfstore')
   , nib = require('nib')
   , util = require('./util')
   , dateFormat = require("date-format-lite")
-  , jsonld = require('jsonld');
+  , jsonld = require('jsonld')
+  , sparqlClient =  require('sparql-client')
+  , async = require('async')
 ;
 
 
@@ -29,10 +31,17 @@ app.use(stylus.middleware(
 ))
 app.use(express.static(__dirname + '/public'))
 
+//register for GET request
+app.get('/', serveRequest);
+
 // The top level object
 //
 var topLevelUri = 'http://lod.isi.edu/course/cs548/2014/spring'
 
+var syllPrefix = 'PREFIX syll: <http://lod.isi.edu/ontology/syllabus/>';	
+
+var foafPrefix = 'PREFIX foaf: <http://xmlns.com/foaf/0.1/>';
+	
 // The files containing the RDF for our web site.
 //
 var rdfFiles = [ 'data/events.n3', 'data/people.n3', 'data/schedule.n3' ];
@@ -43,81 +52,101 @@ var rdfFiles = [ 'data/events.n3', 'data/people.n3', 'data/schedule.n3' ];
 // which is much nicer to deal with than SPARQL result sets.
 //
 var queries = [
-  { 
-    query: "SELECT ?uri ?date { ?uri a syll:Lecture ; syll:hasEventDate ?date . } ORDER BY ?date DESC(?date)" ,
-    callback: function (results, globalData, store) {
-      globalData.lectures = new Object();
-      util.uriToJSONLD(results, globalData.lectures, store);
-    }
-  } ,
-  { 
-    query: "SELECT ?uri ?date { ?uri a syll:Homework ; syll:hasEventDate ?date . } ORDER BY ?date DESC(?date)" ,
-    callback: function (results, globalData, store) {
-      globalData.homeworks = new Object();
-      util.uriToJSONLD(results, globalData.homeworks, store);
-    }
-  } ,
-  { 
-    query: "SELECT ?uri ?date { ?uri a syll:ProjectEvent ; syll:hasEventDate ?date . } ORDER BY ?date DESC(?date)" ,
-    callback: function (results, globalData, store) {
-      globalData.projects = new Object();
-      util.uriToJSONLD(results, globalData.projects, store);
-    }
-  } ,
-  {
-    query: "SELECT ?uri { ?uri a syll:Person . }" ,
-    callback: function (results, globalData, store) {
-      globalData.people = new Object();
-      util.uriToJSONLD(results, globalData.people, store);
-    }
-  } ,
-  {
-    query: "SELECT ?uri { <"+topLevelUri+"> syll:hasInstructor ?uri . ?uri foaf:lastName ?lastName . } ORDER BY ?lastName ASC(?lastName)" ,
-    callback: function (results, globalData, store) {
-      globalData.instructors = new Array();
-      for (var i = 0; i < results.length; i++) {
-        globalData.instructors.push(results[i].uri.value);
-      };
-    }
-  } ,
-  {
-    query: "SELECT ?uri { <"+topLevelUri+"> <http://vivoweb.org/ontology/core#hasPrerequisite> ?uri . }" ,
-    callback: function (results, globalData, store) {
-      globalData.prerequisites = new Object();
-      util.uriToJSONLD(results, globalData.prerequisites, store);
-    }
-  } 
+               { 
+                 query: syllPrefix+" SELECT ?uri ?date { ?uri a syll:Lecture ; syll:hasEventDate ?date . } ORDER BY ?date DESC(?date)" ,
+                 callback: function (results, globalData, store) {
+                   globalData.lectures = new Object();
+                   util.uriToJSONLD(results, globalData.lectures, store);
+                 }
+               } ,
+               { 
+                 query: syllPrefix+" SELECT ?uri ?date { ?uri a syll:Homework ; syll:hasEventDate ?date . } ORDER BY ?date DESC(?date)" ,
+                 callback: function (results, globalData, store) {
+                   globalData.homeworks = new Object();
+                   util.uriToJSONLD(results, globalData.homeworks, store);
+                 }
+               } ,
+               { 
+                 query: syllPrefix+" SELECT ?uri ?date { ?uri a syll:ProjectEvent ; syll:hasEventDate ?date . } ORDER BY ?date DESC(?date)" ,
+                 callback: function (results, globalData, store) {
+                   globalData.projects = new Object();
+                   util.uriToJSONLD(results, globalData.projects, store);
+                 }
+               } ,
+               {
+                 query: syllPrefix+" SELECT ?uri { ?uri a syll:Person . }" ,
+                 callback: function (results, globalData, store) {
+                   globalData.people = new Object();
+                   util.uriToJSONLD(results, globalData.people, store);
+                 }
+               } ,
+               {
+                 query: syllPrefix+" "+foafPrefix+" SELECT ?uri { <"+topLevelUri+"> syll:hasInstructor ?uri . ?uri foaf:lastName ?lastName . } ORDER BY ?lastName ASC(?lastName)" ,
+                 callback: function (results, globalData, store) {
+                   globalData.instructors = new Array();
+                   for (var i = 0; i < results.length; i++) {
+                     globalData.instructors.push(results[i].uri.value);
+                   };
+                 }
+               } , 
+               {
+                 query: "SELECT ?uri { <"+topLevelUri+"> <http://vivoweb.org/ontology/core#hasPrerequisite> ?uri . }" ,
+                 callback: function (results, globalData, store) {
+                   globalData.prerequisites = new Object();
+                   util.uriToJSONLD(results, globalData.prerequisites, store);
+                 }
+               }
 ];
 
+
+//Set up sparql-client stuff
+var endpoint = 'http://localhost:8080/openrdf-sesame/repositories/cs548';
+var client = new sparqlClient(endpoint);
+
 // This is the callback function after we load all the RDF files.
-// It executes the queries to build the JSON data for the web site,
-// and then renders the data using jade.
-//
+// It builds the JSON data for the web site
 function createJsonAndStartApp(store) {
-  var pageData = { store: store };
-
-  store.node(topLevelUri, function(success, graph) {
+	var pageData = { store: store };
+	store.node(topLevelUri, function(success, graph) {
     var doc = server.graphToJSONLD(graph, store.rdf);
-    console.log(doc);
     pageData['topLevel'] = doc[0];
-
-    util.exectueQueries(queries, pageData, store, function(data) {
-      console.log(pageData);
-      app.get('/', function(req, res) {
-        res.render('index', data)
-      });
-      app.listen(3000);
-    });
-  });
+    console.log(pageData);
+    util.setGlobalData(pageData);
+	util.setStore(store);
+	app.listen(3000);
+	console.log("Listening on port 3000");   
+   
+ });
 }
-
-// Main program: create the store and get things going.
+	
+//Main program: create the store and get things going.
 //
 rdfstore.create(function(store) {
   store.registerDefaultProfileNamespaces();
   store.registerDefaultNamespace('syll', 'http://lod.isi.edu/ontology/syllabus/');
   store.registerDefaultNamespace('lecture', 'http://lod.isi.edu/cs548/lecture/')
   util.loadRdfFiles(store, rdfFiles, createJsonAndStartApp);
-}) 
+}) ;
 
 
+//It serves the request for the index page.
+function serveRequest(req,res){
+	var pagedata = util.getGlobalData();
+	var store = util.getStore();
+	async.forEachSeries(queries,function(item,callback_1) {
+		var obj;
+		client.query(item.query).execute(function(error, results) {
+			if(results != null) {
+				obj = JSON.parse(JSON.stringify(results));
+				item.callback(obj.results.bindings,pagedata, store);
+				callback_1(); // Tell async that the current iterator task is finished.
+			}
+			else if(error != null) {
+    			console.log(error);
+    		}
+		});
+	}, function(){ // Called after all the iterator functions have finished
+		//console.log(pagedata);
+		res.render("index",pagedata);
+	});
+}
--- util.js
+++ util.js
@@ -2,7 +2,27 @@ var rdfstore = require('rdfstore')
 	, server = require('./node_modules/rdfstore/server').Server
 	, fs = require('fs');
 
+var dataForTemplate;
+var globalStore;
+
 module.exports = {
+		
+getStore: function() {
+	return globalStore;
+},
+		
+getGlobalData: function() {
+	return dataForTemplate;	
+},	
+
+setGlobalData: function(data) {
+	dataForTemplate = data;
+},
+
+setStore: function(store){
+	globalStore = store;
+},	
+
 	/**
 	 * Load several RDF files in turtle format and call f when they are all loaded.
 	 * @param {store} the rdfstore where we will load the triples.
@@ -16,7 +36,7 @@ module.exports = {
 			var fileName = fileArray[i];
 			var rdf = fs.readFileSync(fileName).toString();
 			store.load('text/turtle', rdf, function(s, d) {
-				console.log("Loaded " + d + " triples.");
+				//console.log("Loaded " + d + " triples.");
 				filesLoaded += 1;
 				if (filesLoaded == fileArray.length) {
 					f(store);
@@ -24,34 +44,17 @@ module.exports = {
 			});
 		}
 	} ,
-
-	exectueQueries: function(queries, pageData, store, f) {
-		var queriesExecuted = 0;
-
-		for (var i = 0; i < queries.length; i++) {
-			console.log("execute\n" + queries[i].query);
-	  	store.execute(queries[i].query, function(success, results) {
-      	//console.log(results);
-      	queriesExecuted += 1;
-      	queries[i].callback(results, pageData, store);
-      	if (queriesExecuted == queries.length) {
-					f(pageData);
-				}
-      });
-    }
-  } ,
-
+    
   uriToJSONLD: function (results, object, store) {
 	  object['uris'] = new Array();
 	  for(var i = 0; i < results.length; i++) {
 	    var uri = results[i].uri.value;
 	    store.node(uri, function(success, graph) {
 	      var doc = server.graphToJSONLD(graph, store.rdf);
-	      console.log(doc);
+	    // console.log(doc);
 	      object[uri] = doc[0];
 	      object['uris'].push(uri);
 	    });
 	  }
-	}
+	} 		
 };
-
